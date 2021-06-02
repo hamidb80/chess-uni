06be2ee8011770836c09583c9b3be407ec03cb2b
hamidb80 <hr.bolouri@gmail.com>
2021-06-02 18:19:36 +0430

    add click handler for chess cells

---
+++ chess-uni/MyForm.h
@@ -0,0 +1,92 @@
+#pragma once
+#include <functional>
+
+namespace chessuni {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	int a = 2;
+	std::function<void(Object^ sender, EventArgs^ e)> evMaker(int c) {
+		return [](Object^ sender, EventArgs^ e)->void {
+			int b = 2;
+			b++;
+		};
+	}
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(175, 192);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(644, 370);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+
+			auto ev = evMaker(9);
+			this->button1->Click += gcnew System::EventHandler(this, &ev);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(14, 29);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1122, 690);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(Object^ sender, EventArgs^ e) {
+	}
+	};
+}
--- chess-uni/UI/game.h
+++ chess-uni/UI/game.h
@@ -11,63 +11,93 @@ namespace UI {
 	using namespace System::Threading;
 	using DSize = System::Drawing::Size;
 
-	public ref class GamePage : public Form
-	{
-	public:
-		GamePage(void)
+	generic<class Type>
+		ref class lambdaWrapper {
+		public:
+			Type valToPass;
+			Action<Type>^ nextFunc;
+			void func(Object^ sender, EventArgs^ e) {
+				nextFunc(valToPass);
+			}
+		};
+
+		public ref class GamePage : public Form
 		{
-			InitializeComponent();
-		}
-		~GamePage()
-		{
-			if (components)
-				delete components;
-		}
+		public:
+			GamePage(void)
+			{
+				InitializeComponent();
+			}
+			~GamePage()
+			{
+				if (components)
+					delete components;
+			}
 
-	private:
-		IContainer^ components;
+		private:
+			IContainer^ components;
 
-		array<Button^>^ boardBtns = gcnew array<Button^>(64);
-		Timer^ timer;
+			Label^ lastAction;
+			array<Button^>^ boardBtns = gcnew array<Button^>(64);
+			Timer^ timer;
 
-		void InitializeComponent(void)
-		{
-			// create window
-			this->AutoScaleDimensions = SizeF(14, 29);
-			this->ClientSize = DSize(800, 600);
-			this->Text = L"main windows";
+			void whenClicked(Point p) {
+				// it shows the position like "(x, y)"
+				lastAction->Text =
+					L"(" + p.X.ToString() + ", " + p.Y.ToString() + L")";
+			}
+			void InitializeComponent(void)
+			{
+				// create window
+				this->AutoScaleDimensions = SizeF(14, 29);
+				this->ClientSize = DSize(800, 600);
+				this->Text = L"main window";
 
-			// create chess board
-			int
-				size = 50,
-				offsetY = 100,
-				offsetX = 100;
+				// create chess board
+				int
+					size = 50,
+					offsetY = 100,
+					offsetX = 50;
+				
+				for (int y = 0; y < 8; y++)
+					for (int x = 0; x < 8; x++) {
+						auto btn = boardBtns[y * 8 + x] = gcnew Button();
+							
+						btn->Location = Point(offsetX + size * x, offsetY + size * y);
+						btn->Size = DSize(size, size);
+						btn->Text = L"X";
+						btn->UseVisualStyleBackColor = true;
 
-			for (int y = 0; y < 8; y++)
-				for (int x = 0; x < 8; x++) {
-					auto btn = boardBtns[y * 8 + x] = gcnew Button();
+						auto lw = gcnew lambdaWrapper<Point>();
+						lw->valToPass = Point(x, y);
+						lw->nextFunc = gcnew Action<Point>(this, &GamePage::whenClicked);
+						btn->Click += gcnew EventHandler(lw, &lambdaWrapper<Point>::func);
+						
+						this->Controls->Add(btn);
+					}
 
-					btn->Location = Point(offsetY + size * y, offsetX + size * x);
-					btn->Size = DSize(size, size);
-					btn->Text = L"btn1";
-					btn->UseVisualStyleBackColor = true;
+				// timer
+				timer = gcnew UI::Timer(this, offsetX / 2, offsetY / 2);
+				timer->setTime(5 * 60 * 60);
 
-					this->Controls->Add(btn);
-				}
+				// label
+				lastAction = gcnew Label();
+				lastAction->Location = Point(20, 10);
+				lastAction->AutoSize = true;
+				lastAction->Text = L"None";
 
-			// create timer
-			timer = gcnew UI::Timer(this, offsetX / 2, offsetY / 2);
-			timer->setTime(5 * 60 * 60);
+				this->Controls->Add(lastAction);
 
-			// set form events
-			this->Load += gcnew EventHandler(this, &GamePage::OnLoad);
-			this->FormClosing += gcnew FormClosingEventHandler(this, &GamePage::OnClosed);
-		}
-		void OnLoad(Object^ sender, EventArgs^ e) {
-			timer->start();
-		}
-		void OnClosed(Object^ sender, FormClosingEventArgs^ e) {
-			timer->stop();
-		}
-	};
+				// set form events
+				this->Load += gcnew EventHandler(this, &GamePage::OnLoad);
+				this->FormClosing += gcnew FormClosingEventHandler(this, &GamePage::OnClosed);
+			}
+
+			void OnLoad(Object^ sender, EventArgs^ e) {
+				timer->start();
+			}
+			void OnClosed(Object^ sender, FormClosingEventArgs^ e) {
+				timer->stop();
+			}
+		};
 }
