7d291822935b8dba7caee0a839f6de348c31ebf2
hamidb80 <hr.bolouri@gmail.com>
2021-07-14 22:02:06 +0430

    od int fs

--- chess-uni/main.cpp
+++ chess-uni/main.cpp
@@ -3,6 +3,7 @@
 using namespace System;
 using namespace System::Windows::Forms;
 
+[STAThreadAttribute]
 void main(){
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
